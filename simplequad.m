d00eb6e181ce1f86fcf89ad76b719d1b8694030f
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-18 18:48:46 +0300

    Add files via upload

---
+++ simplequad.m
@@ -0,0 +1,13 @@
+function [x1,x2]=simplequad(a,b,c)
+%solve quadractic equations a*x^2+b*x+c=0
+%usage:[x1,x2]=simplequad(a,b,c)
+%Written by Nalule Tryphine Ruth 18/oct/2022
+
+
+
+x1=(-b+sqrt(b^2-4*a*c))/(2*a);
+x2=(-b-sqrt(b^2-4*a*c))/(2*a);
+end 
+%%%%%%%%%end of function%%%%%%%%%%
+
+
