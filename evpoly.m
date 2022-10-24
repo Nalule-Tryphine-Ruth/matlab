61aa062768315fb8a69cdcc9544a8236aade0369
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-24 13:29:09 +0300

    Add files via upload

---
+++ evpoly.m
@@ -0,0 +1,14 @@
+clear all;
+close all;
+
+%evaluating 
+%x = x^3-6*x^2-72*x+27;
+%x = [1 3 5 7 9];
+%f= polyval(p,x)
+%f;
+%finding polyynomial roots
+%p = [1 -6 -72 27]
+%r = roots(p)
+
+r=[1 4 2 2]
+p=poly(r)
