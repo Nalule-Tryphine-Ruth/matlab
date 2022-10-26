bda617e665eebe99de22997bd2b1d2a83b95ce8c
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-26 18:18:37 +0300

    Update evpoly.m

--- evpoly.m
+++ evpoly.m
@@ -5,10 +5,13 @@ close all;
 x = x^3-6*x^2-72*x+27;
 x = [1 3 5 7 9];
 f= polyval(p,x)
+% the polyval evaluates a polynomial for a given set of x values
 f;
 finding polyynomial roots
 p = [1 -6 -72 27]
 r = roots(p)
+% calculates the roots of single-variable polynomial represented by a vector of coefficients
 
 r=[1 4 2 2]
 p=poly(r)
+% poly converts the roots back to polynomial coefficients
