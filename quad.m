39f6520e1a4989f893ed3e48c98c0575dd935d0f
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-26 18:29:05 +0300

    Update quad.m

--- quad.m
+++ quad.m
@@ -2,5 +2,6 @@
 [xx1 ,xx2]=simplequad(5,11,3)
 [xx1 ,xx2]=simplequad(1,3,200)
 [xx1 ,xx2]=simplequad(1,-3,-2)
-% this is a continution 
+% this is a continution of the simplequad file 
+% it is only with the help of the simplequad file that the quad file functions
 
