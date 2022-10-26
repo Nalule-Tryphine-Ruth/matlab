9ce38c3b52059d77470b3ef3e896e8c9d149531e
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-26 18:25:07 +0300

    Update norm.m

--- norm.m
+++ norm.m
@@ -1,4 +1,6 @@
 p=[1 -6 3 10]
 x=linspace(-2,6);
+% linscape gives direct control over the number of points and always includes the end points
 y=polyval(p,x);
+% polyval evaluates a polynomial for a  given set of x values
 plot(x,y)
